adb7f33f96975ed64d6c9f658bd615fe3f2f1482
frankyu02 <frankyu02@yahoo.ca>
2025-02-13 16:08:22 -0500

    add death animation

--- Scripts/base_piece.gd
+++ Scripts/base_piece.gd
@@ -4,15 +4,20 @@ extends Node2D
 signal died(pieceId: int)
 @onready var cap_base: BaseCap = get_node("CapBase")
 @onready var killCollission: CharacterBody2D = CharacterBody2D.new()
-
+var tween: Tween = null
 #func startTurn() -> void:
 	#print("starting")
 	#cap_base.startTurn()
 func kill() -> void:
 	print("Working")
 	var id = self.get_instance_id()
-	self.queue_free()
-	died.emit(id)
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.parallel().tween_property(cap_base, "linear_velocity", Vector2(), 0)
+	tween.parallel().tween_property(cap_base.spriteNode, "scale", Vector2(), 1)
+	tween.tween_callback(self.queue_free)
+	tween.tween_callback(func (): self.died.emit(id))
 
 func _ready() -> void:
 	killCollission.set_collision_layer_value(10, true)
